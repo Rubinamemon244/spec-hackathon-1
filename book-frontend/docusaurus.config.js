f122037c48bc2207bbbee38c08331f7228f70929
Rubina Memon <168368987+Rubinamemon244@users.noreply.github.com>
2025-12-23 07:10:18 +0500

    Update

--- book-frontend/docusaurus.config.js
+++ book-frontend/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://speckit-hackathon-1.vercel.app/',
+  url: 'https://vercel.com/rubina-memons-projects/spec-hackathon-1',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
