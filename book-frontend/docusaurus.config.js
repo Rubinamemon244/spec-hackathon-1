b62d86977faa15f0c0c707386f1d937a900cf2be
Rubina Memon <168368987+Rubinamemon244@users.noreply.github.com>
2025-12-23 07:15:32 +0500

    production URL in docusaurus.config.js

--- book-frontend/docusaurus.config.js
+++ book-frontend/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://vercel.com/rubina-memons-projects/spec-hackathon-1',
+  url: 'https://spec-hackathon-1-six.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
